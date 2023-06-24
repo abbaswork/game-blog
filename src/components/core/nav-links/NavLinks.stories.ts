3727cc70c86db19ba835cfbf6db97a51b95eab2f
Abbas Mac <abbasmac@Abbass-iMac.local>
2023-06-23 23:53:42 -0700

    feat: create header/mobile links

---
+++ src/components/core/nav-links/NavLinks.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { NavLinks } from './NavLinks';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof NavLinks> = {
+  title: 'Core/NavLink',
+  component: NavLinks,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof NavLinks>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {};
+
