bb7304327ea8e5d9de0d31dfd4596affac300894
Abbas Mac <abbasmac@Abbass-iMac.local>
2023-07-09 05:00:11 -0700

    feat: convert ts files to tsx

--- src/components/core/nav-links/NavLinks.stories.tsx
+++ src/components/core/nav-links/NavLinks.stories.tsx
@@ -5,13 +5,12 @@ import { NavLinks } from './NavLinks';
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof NavLinks> = {
   title: 'Core/NavLink',
-  component: NavLinks,
-  tags: ['autodocs'],
+  component: NavLinks
 };
 
 export default meta;
 type Story = StoryObj<typeof NavLinks>;
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+// render empty decorators
 export const Default: Story = {};
 
