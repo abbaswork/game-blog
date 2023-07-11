f35bd99c9475c7dad7ee9a86d9c2f60a4d1592de
Abbas Mac <abbasmac@Abbass-iMac.local>
2023-07-11 16:53:12 -0700

    feat: create side panel for sidebar and add into page layout

--- src/components/layouts/page/Page.tsx
+++ src/components/layouts/page/Page.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './page.scss';
 import { Header } from './../../header/Header';
 import { PageContent } from '../page-content/PageContent';
+import { SidePanel } from '../side-panel/SidePanel';
+import { ListContainer } from './../../core/list-container/ListContainer';
+import { HeroImage } from './../../core/hero-image/HeroImage';
 
 export const Page: React.FC = () => {
 
@@ -9,8 +12,30 @@ export const Page: React.FC = () => {
     <>
       <Header />
       <div className='page-layout'>
-        {/* Consider using section + article tags */}
-        <PageContent />
+        {/* Render Basic Blog Content*/}
+        <PageContent>
+          <HeroImage />
+          <h1>Page Title</h1>
+          <p>Blog Introduction</p>
+          <ListContainer title='Table of Contents'>
+            <>
+              <li><a>Section 1</a></li>
+              <li><a>Section 1</a></li>
+              <li><a>Section 1</a></li>
+            </>
+          </ListContainer>
+          <h2>Subheading</h2>
+          <p>Blog Content</p>
+        </PageContent>
+
+        {/* Render Sidebar in sidepanel */}
+        <SidePanel>
+          <ListContainer title='Sidebar'>
+            <>
+              <li>New Blogs Coming Soon</li>
+            </>
+          </ListContainer>
+        </SidePanel>
       </div>
     </>
   );
