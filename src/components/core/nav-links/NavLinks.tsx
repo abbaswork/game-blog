8b66a71f66c78bab59d86e4237c09a559e89c445
Abbas Mac <abbasmac@Abbass-iMac.local>
2023-07-16 07:49:29 -0700

    fix: svg fixes

--- src/components/core/nav-links/NavLinks.tsx
+++ src/components/core/nav-links/NavLinks.tsx
@@ -1,25 +1,30 @@
+'use client';
+
 import React from 'react';
 import './nav-links.scss';
 import { useRef } from "react"
 import { useOutsideRefClick } from './../../util/util';
+
+//client component given the link behaviour
 
 export const NavLinks = () => {
 
   //button that opens mobile overlay
   const [showMobileOverlay, setshowMobileOverlay] = React.useState<boolean>(false);
 
-    //set ref, pass handler for setting the dropdown when clicked outside of the ref
-    const overlayRef = useRef(null);
-    useOutsideRefClick(overlayRef, () => setshowMobileOverlay(false));
+  //set ref, pass handler for setting the dropdown when clicked outside of the ref
+  const overlayRef = useRef(null);
+  
+  useOutsideRefClick(overlayRef, () => setshowMobileOverlay(false));
 
   return (
     <div className={(showMobileOverlay ? " mobile-overlay" : "")}>
       {/* Container that determines how it's children styles change in the mobile overlay ^ */}
-      
+
       {/* Button that opens overlay */}
       <div className='mobile-bars' onClick={() => setshowMobileOverlay(!showMobileOverlay)}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
       </div>
 
--- src/components/core/searchbar/Searchbar.tsx
+++ src/components/core/searchbar/Searchbar.tsx
@@ -5,8 +5,8 @@ import './searchbar.scss';
 export const Searchbar = () => {
   return (
     <div className='searchbar-container'>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
       </svg>
       <input className='searchbar' type='text' placeholder='search' />
     </div>
