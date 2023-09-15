6481e182b71ac851e6416cdbf7005578f8d82eb4
Abbas Mac <abbasmac@Abbass-iMac.local>
2023-09-14 20:57:42 -0700

    feat: header integration for blog pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Page } from '@/types';
 import PageService from '@/services/page/parsePage';
 import { Metadata } from 'next';
+import { wpPreviewHeaders } from '@/config/api';
 
 //get blog data fetch
 async function getPage(slug: string): Promise<PageService> {
-  const pageFetch: Page[] = await fetch(`http://ec2-18-213-34-154.compute-1.amazonaws.com/wp-json/wp/v2/pages?slug=${slug}&per_page=1`, { next: { revalidate: 0 } }).then((res) => res.json());
+  const pageFetch: Page[] = await fetch(`${process.env.WP_PROTOCOL}://${process.env.WP_DOMAIN}/wp-json/wp/v2/pages?slug=${slug}&per_page=1`,
+    {
+      headers: wpPreviewHeaders,
+      next: { revalidate: 0 }
+    }
+  ).then((res) => res.json());
   const pageRender = new PageService(pageFetch[0]);
   return pageRender;
 }
@@ -12,7 +18,7 @@ async function getPage(slug: string): Promise<PageService> {
 export const metadata: Metadata = {
   title: "Metric Gamer: curated list of ranked games for PS2 and more",
   description: "Checkout our list of best games for modern and older consoles",
-  alternates : {
+  alternates: {
     canonical: "/"
   }
 }
