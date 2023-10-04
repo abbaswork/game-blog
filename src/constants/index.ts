213dded5bcb718aef5a41b5dfde7ba10df5a431d
abbaswork <abbaskhanwork@gmail.com>
2023-10-04 16:46:58 -0400

    feat: move constants to constant file

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+/**
+ * classes that represent elements that have components meant to replace them
+ */
+export enum WPTags {
+    FeatureBlogImage = "wp-block-post-featured-image",
+    PageImage = "wp-block-image",
+    TitleWithRating = "title-with-rating",
+    PostCard = "post type-post status-publish",
+    RatingList = "rating-list",
+    GameTags = "game-tags"
+}
