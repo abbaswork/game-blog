2e501d500de09ee9de915b713f814d12186dc6a4
Abbas Khan <abbaskhan@Abbass-MacBook-Air.local>
2023-11-25 15:00:16 -0500

    feat: create/update navigation service

--- src/services/navigation/navigation.tsx
+++ src/services/navigation/navigation.tsx
@@ -1,45 +1,31 @@
-import { wpPreviewHeaders } from '@/config/api';
+import WordpressApi from '@/services/api/wordpress';
 import { transformTitleUrl } from '../utils';
 import { NavSearchType, blogItem, menuItem, menuLinkProps, searchResults, tagItem } from './types';
 
 /**
  * This service handles navigation operations so that it can be used within any component
- * It leverages Next to cache and optimize performance as much as possible
- * Strategy:
- * - cache operations that don't change on the server side (this means this service needs to be rendered in a server side component or page/layout file)
- * - functions that need to be rendered within a component can leverage the existing data
+ * @description: It leverages WP Rest API & Next to handle caching of the requests
+ * Functions can be run on the server side (cached) OR client side with little performance hit
  */
 export default class NavigationService {
 
-    //default page params
-    menuLinks: menuLinkProps[];
-    tags: tagItem[];
+    API = new WordpressApi();
 
     /**
-     * Initialise and parse default params
-     * @param post 
-     * @param parseContentOptions - useful for specific pages and classes that extend page
+     * parse menu props from menu items
+     * @param menuItems 
+     * @returns 
      */
-    constructor(menuItems: menuItem[], tags: tagItem[]) {
-        this.menuLinks = this.parseLinks(menuItems);
-        this.tags = tags;
+    getMenuItems = async (): Promise<menuLinkProps[]> => {
 
-        console.log("menuLinks: ", menuItems[0]._links);
+        //get menu items
+        const menuItems = await this.API.getMenu();
 
-        // blogItems.map(blog => console.log({
-        //     generated_slug: blog.generated_slug,
-        //     meta: blog.meta,
-        //     tags: blog.tags,
-        //     categories: blog.categories
-        // }))
-    }
-
-    //parse meta properties from wp pages
-    parseLinks = (menuItems: menuItem[]): menuLinkProps[] => {
-
+        //check if valid
         if (!menuItems || menuItems.length < 1)
             return [];
 
+        //parse menu items and generate slug
         const parsedMenuItems: menuLinkProps[] = menuItems.map(item => {
             return {
                 text: item.title.rendered,
@@ -50,36 +36,19 @@ export default class NavigationService {
         return parsedMenuItems;
     }
 
-    // Query Search API using fetch <- cached with Next, used for search results
-    searchBlogs = async (search: string): Promise<searchResults[]> => {
 
-        if (search.length < 3)
-            return [];
+    /**
+     * Function that returns search results by searching titles and content
+     * @param search 
+     * @returns 
+     */
+    searchBlogs = (search: string): Promise<searchResults[]> | null => {
 
-        const blogsMetaFetch: blogItem[] = await fetch(
-            `${process.env.WP_PROTOCOL}://${process.env.WP_DOMAIN}/wp-json/wp/v2/search?subtype="post"&search="${search}"`,
-            {
-                headers: wpPreviewHeaders,
-                next: { revalidate: 0 }
-            }
-        ).then((res) => res.json())
-            .catch(e => console.log('e: ', e));
+        //must have at least 3 letters typed out
+        if (search.length < 2)
+            return null;
 
-        console.log("blogsMetaFetch: ", blogsMetaFetch);
-
-        return [];
-    }
-
-    getCurrentMeta = async () => {
-
-        const blogsMetaFetch: blogItem[] = await fetch(
-            `${process.env.WP_PROTOCOL}://${process.env.WP_DOMAIN}/wp-json/wp/v2/post?_filter=""`,
-            {
-                headers: wpPreviewHeaders,
-                next: { revalidate: 0 }
-            }
-        ).then((res) => res.json())
-            .catch(e => console.log('e: ', e));
+        return this.API.searchPosts(search);
 
     }
 }
