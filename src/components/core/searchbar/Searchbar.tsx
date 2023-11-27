8f2fa181b932dd2245f631286797dfac9374db3c
Abbas Khan <abbaskhan@Abbass-MacBook-Air.local>
2023-11-26 21:07:33 -0500

    fix: disable searchbar, http over https error

--- src/components/core/searchbar/Searchbar.tsx
+++ src/components/core/searchbar/Searchbar.tsx
@@ -43,7 +43,7 @@ export const Searchbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
         </svg>
-        <input onChange={handleInputChange} className={"searchbar"} type="text" placeholder="search list/game" />
+        <input disabled onChange={handleInputChange} className={"searchbar"} type="text" placeholder="search list/game" />
         <div className={data ? "searchbar-dropdown content-visible" : "searchbar-dropdown"}>
           {mapSearchResults()}
         </div>
